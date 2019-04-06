d1eab51433a83be93921297b7fcf4bd456681adf
Zhakshylyk <alymkulov64@gmail.com>
2019-04-06 18:59:16 +0600

    final h/w for 4th lesson

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,18 +2,23 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        int name = 2;
         String names;
-        switch (name) {
-            case 1:  names = "Ilon1 ";
-                System.out.println(names + "Доброе утро!");
-                break;
-            case 2:  names = "Ilon2 ";
-                System.out.println(names + "Добрый день!");
-                break;
-            case 3:  names = "Ilon3 ";
-                System.out.println(names + "Добрый вечер!");
-                break;
+        for(int name = 0; name <= 3; name++) {
+            switch (name) {
+                case 1:
+                    names = "Ilon1 ";
+                    System.out.println(names + "Доброе утро!");
+                    break;
+                case 2:
+                    names = "Ilon2 ";
+                    System.out.println(names + "Добрый день!");
+                    break;
+                case 3:
+                    names = "Ilon3 ";
+                    System.out.println(names + "Добрый вечер!");
+                    break;
+            }
         }
+
     }
 }
